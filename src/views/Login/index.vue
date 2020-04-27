57b342ac8f2761fb0c86d94a12a283f3c04d3376
zhang98914 <1091668231@qq.com>
2020-04-27 17:16:14 +0800

    构建项目、配置全局样式、路由。elementui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="login">这是登录页面</div>
+</template>
+<script>
+export default {
+    name:"login"
+}
+</script>
+<style scoped>
+#login{
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
